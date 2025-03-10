28694a30599f716b8ed705d39d0f0b1986741594
Nooooneee <guswpdlf@gmail.com>
2025-03-10 10:43:05 +0900

    basic1

---
+++ BasicStudy1/Assets/Script/VarialbeExample.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class VarialbeExample : MonoBehaviour
+{
+    //변수 선언
+    public int playerScore = 0;
+    public float speed = 5.5f;
+    public string playerName = "Hero";
+    public bool isGameOver = false;
+
+
+    void Start()
+    {
+        Debug.Log($"Player Name : {playerName}");
+        Debug.Log($"Player Score : {playerScore}");
+        Debug.Log($"speed : {speed}");
+        Debug.Log($"Is Game Over? : {isGameOver}");
+    }
+}
